fc1c3910c7d1ccf9acc0bb0896a6b7d76366975d
Davidson Gomes <davidsongviolao@gmail.com>
2022-10-25 17:42:09 -0300

    autenticacao user

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,10 +14,13 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+            // $table->morphs('tokenable');
+            $table->string('tokenable_type');
+            $table->uuid('tokenable_id');
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
+            $table->dateTime('expires_at')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamps();
         });
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Api\{
     ReplySupportController,
     SupportController
 };
+use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,6 +15,9 @@ Route::get('/', function () {
         'success' => true
     ]);
 });
+
+// Login
+Route::post('/auth', [AuthController::class, 'auth'])->name('login');
 
 // Courses
 Route::get('/courses', [CourseController::class, 'index'])->name('courses.index');
