2d386c56d5ba1ad08803d19a66b4b557a7e43146
Davidson Gomes <davidsongviolao@gmail.com>
2022-10-26 12:35:57 -0300

    reset password send email

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -41,7 +41,7 @@ class AuthController extends Controller
 
     public function logout()
     {
-        $user = auth()->user();
+        $user = User::find(auth()->user()->id);
 
         $user->tokens()->delete();
 
--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,10 @@ use App\Http\Controllers\Api\{
     ReplySupportController,
     SupportController
 };
-use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Auth\{
+    AuthController,
+    ResetPasswordController
+};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,15 +19,23 @@ Route::get('/', function () {
     ]);
 });
 
+/**
+ * Auth
+ */
 // Login
 Route::post('/auth', [AuthController::class, 'auth'])->name('login');
-
 
 // Logout
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:sanctum');
 
 // Me
 Route::get('/profile', [AuthController::class, 'profile'])->name('profile')->middleware('auth:sanctum');
+
+/**
+ * Reset Password
+ */
+// Forgot Password
+Route::post('/forgot-password', [ResetPasswordController::class, 'sendResetLink'])->name('forgot-password')->middleware('guest');
 
 Route::middleware(['auth:sanctum'])->group(function () {
     // Courses
